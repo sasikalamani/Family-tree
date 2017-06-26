044968855df8e6c9b69f4541e357938ffe5a8a9e
Sasikala Mani <mani.37@ruan.cse.ohio-state.edu>
2017-06-26 12:43:04 -0400

    2nd

--- famtre.py
+++ famtre.py
@@ -165,14 +165,18 @@ class MLP(object):
         # L1 norm ; one regularization option is to enforce L1 norm to
         # be small
         self.L1 = (
-            abs(self.hiddenLayer.W).sum()
+            abs(self.hiddenLayerL.W).sum()
+            + abs(self.hiddenLayerR.W).sum()
+            + abs(self.hiddenLayerTop.W).sum()
             + abs(self.logRegressionLayer.W).sum()
         )
 
         # square of L2 norm ; one regularization option is to enforce
         # square of L2 norm to be small
         self.L2_sqr = (
-            (self.hiddenLayer.W ** 2).sum()
+            (self.hiddenLayerL.W ** 2).sum()
+            + (self.hiddenLayerR.W ** 2).sum()
+            + (self.hiddenLayerTop.W ** 2).sum()
             + (self.logRegressionLayer.W ** 2).sum()
         )
 
@@ -187,7 +191,7 @@ class MLP(object):
 
         # the parameters of the model are the parameters of the two layer it is
         # made out of
-        self.params = self.hiddenLayer.params + self.logRegressionLayer.params
+        self.params = self.hiddenLayerR.params + self.logRegressionLayer.params
         # end-snippet-3
 
         # keep track of model input
@@ -225,9 +229,9 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
    """
 
     #print(trainO)
-    Xo = theano.shared(value=np.asarray(array, dtype='float64'), name='Xo')
-    Xo1 = theano.shared(value=np.asarray(array2, dtype='float64'), name='Xo')
-    yo = theano.shared(value=np.asarray(output, dtype = 'int32'), name='yo')
+    #Xo = theano.shared(value=np.asarray(array, dtype='float64'), name='Xo')
+    #Xo1 = theano.shared(value=np.asarray(array2, dtype='float64'), name='Xo')
+    #yo = theano.shared(value=np.asarray(output, dtype = 'int32'), name='yo')
     #Xot = theano.shared(value=np.asarray(array, dtype='float64'), name='Xot')
     #yot = theano.shared(value=np.asarray(output, dtype = 'int32'), name='yot')
     #Xov = theano.shared(value=np.asarray(array, dtype='float64'), name='Xot')
@@ -235,9 +239,9 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
 
     #print(y)
 #    sys.exit()
-    train_set_x, train_set_x1, train_set_y = (Xo, Xo1, yo)
-    valid_set_x, valid_set_y = (Xo,yo)
-    test_set_x, test_set_y = (Xo,yo)
+    train_set_x, train_set_x1, train_set_y = (array, array2, output)
+    valid_set_x, valid_set_x1, valid_set_y  = (array, array2, output)
+    test_set_x, test_set_x1, test_set_y = (array, array2, output)
 
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
