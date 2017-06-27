0903e3025044a5ecff6f09d1e94ea5ea2e67a3d8
Sasikala Mani <mani.37@ruan.cse.ohio-state.edu>
2017-06-27 11:54:43 -0400

    train/test bug

--- famtre.py
+++ famtre.py
@@ -18,7 +18,8 @@ import preproc
 from logistic_sgd import LogisticRegression
 
 
-(array, array2, output) = preproc.data()
+(array, array2, output, testA, testB, testF) = preproc.data()
+print(len(array2))
 
 # start-snippet-1
 class HiddenLayer(object):
@@ -137,19 +138,19 @@ class MLP(object):
             rng=rng,
             input=inputA,
             n_in=n_inA,
-            n_out=n_hidden,
+            n_out=6,
             activation=T.tanh
         )
         self.hiddenLayerR = HiddenLayer(
             rng = rng,
             input=inputB, 
             n_in = n_inB,
-            n_out = n_hidden,
+            n_out = 6,
             activation = T.tanh
         )
         self.hiddenLayerTop = HiddenLayer(
             rng = rng, 
-            input = T.concatenate([self.hiddenLayerL.output,self.hiddenLayerR.output]),
+            input = T.concatenate([self.hiddenLayerL.output,self.hiddenLayerR.output],1),
             n_in = 12,
             n_out = 12,
             activation = T.tanh)
@@ -158,7 +159,7 @@ class MLP(object):
         # of the hidden layer
         self.logRegressionLayer = LogisticRegression(
             input=self.hiddenLayerTop.output,
-            n_in=n_hidden,
+            n_in=12,
             n_out=n_out
         )
         # end-snippet-2 start-snippet-3
@@ -199,7 +200,7 @@ class MLP(object):
 
 
 def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
-             dataset='mnist.pkl.gz', batch_size=20, n_hidden=102):
+             dataset='mnist.pkl.gz', batch_size=20, n_hidden=6):
     """
     Demonstrate stochastic gradient descent optimization for a multilayer
     perceptron
@@ -232,6 +233,9 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
     Xo = theano.shared(value=array, name='Xo')
     Xo1 = theano.shared(value=array2, name='Xo')
     yo = theano.shared(value=output, name='yo')
+    Xo1 = theano.shared(value=testA, name='Xo1')
+    Xo2 = theano.shared(value=testB, name='Xo2')
+    yo1 = theano.shared(value=testF, name='yo1')
     #Xot = theano.shared(value=np.asarray(array, dtype='float64'), name='Xot')
     #yot = theano.shared(value=np.asarray(output, dtype = 'int32'), name='yot')
     #Xov = theano.shared(value=np.asarray(array, dtype='float64'), name='Xot')
@@ -240,8 +244,8 @@ def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
     #print(y)
 #    sys.exit()
     train_set_x, train_set_x1, train_set_y = (Xo, Xo1, yo)
-    valid_set_x, valid_set_x1, valid_set_y  = (Xo, Xo1, yo)
-    test_set_x, test_set_x1, test_set_y = (Xo, Xo1, yo)
+    valid_set_x, valid_set_x1, valid_set_y  = (Xo1, Xo2, yo1)
+    test_set_x, test_set_x1, test_set_y = (Xo1, Xo2, yo1)
 
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
--- preproc.py
+++ preproc.py
@@ -36,10 +36,13 @@ m = len(dataset_as_dict)
 
 # Encoding the dataset into input / target pairs as specified in the paper.
 input_vectors = np.zeros((m, 36), dtype='float64')
-input_vectorsA = np.zeros((m, 24), dtype='float64')
-input_vectorsB = np.zeros((m, 12), dtype='float64')
+input_vectorsA = np.zeros((100, 24), dtype='float64')
+input_vectorsB = np.zeros((100, 12), dtype='float64')
 target_vectors = np.zeros((m, 24), dtype = 'float64')
-target_vectors_f = np.zeros((m), dtype = 'int32')
+target_vectors_f = np.zeros((100), dtype = 'int32')
+test_A = np.zeros((4, 24), dtype='float64')
+test_B = np.zeros((4, 12), dtype='float64')
+test_target = np.zeros((4), dtype = 'int32')
 
 for i, tup in enumerate(dataset_as_dict.items()):
     input_ = tup[0]
@@ -49,6 +52,14 @@ for i, tup in enumerate(dataset_as_dict.items()):
     for relative in target:
         target_vectors[i, person_mapping[relative]] = 1
 
+np.random.seed(2)
+
+test_set_ids = np.random.randint(m, size=4)
+test_set, test_set_f= input_vectors[test_set_ids, :], target_vectors[test_set_ids, :]
+input_vectors = np.delete(input_vectors, test_set_ids, 0)
+target_vectors = np.delete(target_vectors, test_set_ids, 0)
+m = len(input_vectors)
+
 def data():
     for i in range(m):
         input_vectorsA[i] = input_vectors[i][0:24]
@@ -56,6 +67,12 @@ def data():
         for j in range(24):
             if(target_vectors[i][j] == 1.0):
                 target_vectors_f[i] = int(j)
-    return(input_vectorsA , input_vectorsB, target_vectors_f)
-
+    for i in range(4):
+        test_A[i] = test_set[i][0:24]
+        test_B[i] = test_set[i][24:]
+        for j in range(24):
+            if(test_set_f[i][j] == 1.0):
+                test_target[i] = int(j)
+    return(input_vectorsA, input_vectorsB, target_vectors_f,
+        test_A, test_B, test_target)
 
